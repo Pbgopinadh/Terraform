1ded81e1036c970302f98d22c92b469c34f43a27
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-29 17:05:58 +0530

    Terraform scripts

--- infra.tf
+++ infra.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" { # resouce is where we declare the type of instance we want to create and the local name of it.
   ami           = "ami-066784287e358dad1"
   instance_type = "t2.micro" # these are arugments and this whole {.........} is called a block
+  vpc_security_group_ids = "sg-0f32ca9e850ae332e"
 
   tags = {
     Name = "fromterraform" # name of the instance.
