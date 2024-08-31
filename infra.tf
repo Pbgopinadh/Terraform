4a7ab74aae0cbd57cedc46e577511110a11e4f0a
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-31 20:52:24 +0530

    updating script

--- infra.tf
+++ infra.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" { # resouce is where we declare the type of instance we want to create and the local name of it.
-  count = lenght(counts) # lenth gets the lenght of the variable dynamically. #3 instances will be created using this function this can be considered as looping mechanism in Terraform.
+  count = length(counts) # lenth gets the lenght of the variable dynamically. #3 instances will be created using this function this can be considered as looping mechanism in Terraform.
   ami           = "ami-066784287e358dad1"
   instance_type = "t2.micro" # these are arugments and this whole {.........} is called a block
   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
