8ec60690d5d370c0c962efdea360cbc489f7d65e
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-31 22:53:55 +0530

    updating script

--- condition.tf
+++ condition.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instance1" { 
+resource "aws_instance" "instance2" { 
   ami           = "ami-066784287e358dad1"
   instance_type = var.comp == "dev" ? t3.medium : "t3.micro" # here for each key there can be multiple values so for that key the are taken as each.value["value to be retreived"]
   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
