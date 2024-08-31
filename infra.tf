b65dcf491a611aeb1c837f715ebec5905c735423
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-31 20:37:30 +0530

    updating script

--- infra.tf
+++ infra.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "instance" { # resouce is where we declare the type of instance we want to create and the local name of it.
-  count = 3
+  count = 3 #3 instances will be created using this function this can be considered as looping mechanism in Terraform.
   ami           = "ami-066784287e358dad1"
   instance_type = "t2.micro" # these are arugments and this whole {.........} is called a block
   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
 
   tags = {
-    Name = "terrcreate-${count.index}" # name of the instance.
+    Name = "terrcreate-${count.index+1}" # name of the instance. #count is terraform function and this cound has a property known as index where its give the iteration number count.index = 0,1,2,3,4......
+    # even arthimetic functions are supported.
   }
 }
 
