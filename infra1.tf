4e340aacfa37cb8f014efd33767875ecf79ff72f
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-31 22:27:37 +0530

    updating script

--- infra1.tf
+++ infra1.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "instance1" { 
   for_each = var.components
   ami           = "ami-066784287e358dad1"
-  instance_type = "${each.value["instance"]}" # these are arugments and this whole {.........} is called a block
+  instance_type = "${each.value["instance"]}" # here for each key there can be multiple values so for that key the are taken as each.value["value to be retreived"]
   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
 
   tags = {
-        name  = "${each.key}"
+        name  = "${each.key}" #so here each.key will take the keys of the map 
   }
   }
 
