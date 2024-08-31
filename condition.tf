10996479942ea4c881ea984dbce5856520e753c2
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-31 22:57:52 +0530

    updating script

--- condition.tf
+++ condition.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "instance2" { 
   ami           = "ami-066784287e358dad1"
-  instance_type = var.comp == "dev" ? t3.medium : "t3.micro" # here for each key there can be multiple values so for that key the are taken as each.value["value to be retreived"]
-  vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
+  instance_type = var.comp == "dev" ? "t3.medium" : "t3.micro"  # conditional statement expression ? true : false
+  vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] 
 
   tags = {
-        name  = "condtion" #so here each.key will take the keys of the map 
+        name  = "condtion"
   }
   }
 
