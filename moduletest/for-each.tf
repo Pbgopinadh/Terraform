53022f5da815458080fa6a2b13385f090e0d6700
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-09-01 17:25:23 +0530

    updating script

--- moduletest/for-each.tf
+++ moduletest/for-each.tf
@@ -5,7 +5,7 @@
 #   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] #so if a arugments can have multiple values then we should mention them like this
 
 resource "aws_instance" "instance1" { 
-  for_each = var.components
+  for_each = var.arguments
   ami           = "ami-066784287e358dad1"
   instance_type = each.value["instance"]  
   vpc_security_group_ids = ["sg-0f32ca9e850ae332e"] 
@@ -14,6 +14,6 @@ resource "aws_instance" "instance1" {
   }
   }
 
-variable "components" {}
+variable "arguments" {}
 
 
