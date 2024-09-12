215ae06e5f2e8d8823358dd744ac736f8da8098c
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-09-13 01:00:47 +0530

    updating the scripts

--- terraform_testing/main.tf
+++ terraform_testing/main.tf
@@ -1,6 +1,6 @@
 module "terraform_testing" {
   for_each = var.components
-  source = "git@github.com:Pbgopinadh/terraform_testing.git"
+  source = "git::https://github.com/Pbgopinadh/terraform_testing.git"
   name = "${var.env}"-each.key
   ami = data.aws_ami.ids.image_id
   instance = each.value["instance"]
